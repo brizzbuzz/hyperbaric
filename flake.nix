fba3bc9686129930920c6cdac1be2594eb56802a
Ryan Brink <dev@ryanbr.ink>
2025-08-22 16:38:54 -0400

    feat: initialize repo

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "My oxygen chamber";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bun
+          turbo
+        ];
+      };
+    };
+}
