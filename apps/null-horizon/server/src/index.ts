5ef4f9776945c255ebf17cc286177919c53af1b1
Ryan Brink <dev@ryanbr.ink>
2025-08-22 19:08:02 -0400

    chore: touch up package structure

--- apps/null-horizon/server/src/index.ts
+++ apps/null-horizon/server/src/index.ts
@@ -4,9 +4,4 @@ const app = new Hono();
 
 app.get("/", (c) => c.text("Hello World!"));
 
-const port = parseInt(process.env.PORT || "3001");
-
-export default {
-  port,
-  fetch: app.fetch,
-};
+export default app;
