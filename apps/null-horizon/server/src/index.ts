21f77176a39fdee9aa2786abe8a1a99afa54b272
Ryan Brink <5607577+brizzbuzz@users.noreply.github.com>
2025-08-22 20:54:34 -0400

    feat: auth routes (#6)

--- apps/null-horizon/server/src/index.ts
+++ apps/null-horizon/server/src/index.ts
@@ -1,7 +1,44 @@
 import { Hono } from "hono";
+import { auth } from "./auth";
+import { cors } from "hono/cors";
 
-const app = new Hono();
+const app = new Hono<{
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  };
+}>();
 
 app.get("/", (c) => c.text("Hello World!"));
+
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:3000",
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
+
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
+});
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+  return auth.handler(c.req.raw);
+});
 
 export default app;
